394aac97afbf6155367a17b054aaa0ad5026e501
Quantum-0 <quantum0@mail.ru>
2019-01-30 00:04:27 +0300

    Bugfix

--- src/QuantumButton.cpp
+++ src/QuantumButton.cpp
@@ -158,8 +158,10 @@ uint8_t Button::process_button()
 {
   if (!_onTap && !_on2Tap && !_on3Tap && !_onLTap && !_onHold)
 	  return None;
+  
   bool pin_state = digitalRead(_pin) == _active_state;
   unsigned long current_time = millis();
+  
   if (pin_state == LOW && logical_state == BS_HOLDED_WAIT_FOR_RELEASING)
   {
 	  logical_state = BS_RELEASED;
@@ -169,7 +171,7 @@ uint8_t Button::process_button()
     logical_state = BS_TAP1_PRESSING_BEGIN;
     tap_time = current_time;
   }
-  else if ((pin_state == LOW && (current_time - tap_time > delay_between_taps || (_on2Tap == NULL && _on3Tap == NULL))) || current_time - tap_time > delay_between_taps + very_long_tap_time)
+  else if ((pin_state == LOW && current_time - tap_time > delay_between_taps) || current_time - tap_time > delay_between_taps + very_long_tap_time)
   {
     ButtonEvent result = None;
     if (logical_state == BS_TAP1_RELEASING_END)
@@ -181,8 +183,12 @@ uint8_t Button::process_button()
     else if (logical_state == BS_LONG_TAP)
       result = LongTap;
     else if (logical_state == BS_HOLD)
-      result = VeryLongTap;
-    logical_state = BS_HOLDED_WAIT_FOR_RELEASING;
+	{
+		result = VeryLongTap;
+		logical_state = BS_HOLDED_WAIT_FOR_RELEASING;
+	}
+	if (logical_state != BS_HOLDED_WAIT_FOR_RELEASING)
+		logical_state = BS_RELEASED;
     return result;
   }
   else if (current_time - tap_time > bounce_time)
@@ -190,11 +196,27 @@ uint8_t Button::process_button()
     if (logical_state == BS_TAP1_PRESSING_BEGIN)
       logical_state = BS_TAP1_PRESSING_END;
     else if (logical_state == BS_TAP1_RELEASING_BEGIN)
-      logical_state = BS_TAP1_RELEASING_END;
+	{
+	  if (!(_on2Tap || _on3Tap))
+	  {
+		  logical_state = BS_RELEASED;
+		  return Tap;
+	  }
+	  else
+		  logical_state = BS_TAP1_RELEASING_END;
+	}
     else if (logical_state == BS_TAP2_PRESSING_BEGIN)
       logical_state = BS_TAP2_PRESSING_END;
     else if (logical_state == BS_TAP2_RELEASING_BEGIN)
-      logical_state = BS_TAP2_RELEASING_END;
+	{
+	  if (!_on3Tap)
+	  {
+		  logical_state = BS_RELEASED;
+		  return DoubleTap;
+	  }
+	  else
+		  logical_state = BS_TAP2_RELEASING_END;;
+	}
     else if (logical_state == BS_TAP3_PRESSING_BEGIN)
       logical_state = BS_TAP3_PRESSING_END;
     else if (logical_state == BS_TAP3_RELEASING_BEGIN)
